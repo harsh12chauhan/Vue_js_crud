bded918dc2ac52a68071eae05d2a7c1eef0a0767
Harsh12chauhan <harshchauhan0225@gmail.com>
2023-09-26 12:39:03 +0530

    Added input form validation in createStudent page

--- src/views/CreateStudentView.vue
+++ src/views/CreateStudentView.vue
@@ -1,23 +1,52 @@
 <script>
 import axios from 'axios'
     export default{
+        name:'create_student',
         data(){
             return {
+                error:{
+                    ename:null,
+                    eemail:null,
+                    ephone:null,
+                    eaddress:null
+                },
                 formData:{
                     name:'',
                     email:'',
                     phone:'',
                     address:''
-                }
+                },
+                check:true,
             }
         },
         methods:{
+            validations(){ 
+                this.check = true;
+                if(!this.formData.name){
+                    this.error.ename = true;
+                    this.check = false;
+                }                                    
+                if(!this.formData.email){
+                    this.error.eemail = true;
+                    this.check = false;
+                }                                    
+                if(!this.formData.phone){
+                    this.error.ephone = true;
+                    this.check = false;
+                }                                    
+                if(!this.formData.address){
+                    this.error.eaddress = true;
+                    this.check = false;
+                }                                    
+                return this.check
+            },
            async postUsers() {
-                // console.log(this.formData);
-                await axios.post('http://localhost:3000/data',this.formData)
-                .then(res=>"success")
-                .catch(err=>{err})
-                this.$router.push('/students')
+               if(this.validations()){
+                   await axios.post('http://localhost:3000/data',this.formData)
+                   .then(res=>"success")
+                   .catch(err=>{err})
+                   this.$router.push('/students')
+               }
             }
         }
     }
@@ -29,21 +58,21 @@ import axios from 'axios'
         <div class="left_form">
             <div class="div_form">
                 <label for="name" class="label_form"> Name </label>
-                <input type="text" placeholder="Name" name="name" class="input_form" v-model="formData.name">
+                <input type="text" placeholder="Name" name="name" :class="error.ename && !(formData.name) ?'red_err':'input_form'" v-model="formData.name">
             </div>
             <div class="div_form">
                 <label for="email" class="label_form"> Email </label>
-                <input type="text" placeholder="email" name="email" class="input_form" v-model="formData.email">
+                <input type="text" placeholder="Email" name="email" :class="error.eemail && !(formData.email) ?'red_err':'input_form'" v-model="formData.email">
             </div>
         </div>
         <div class="right_form">
             <div class="div_form">
                 <label for="phone" class="label_form"> Phone </label>
-                <input type="text" placeholder="Phone" name="phone" class="input_form" v-model="formData.phone">
+                <input type="text" placeholder="Phone" name="phone" :class="error.ephone && !(formData.phone) ?'red_err':'input_form'" v-model="formData.phone">
             </div>
             <div class="div_form">
                 <label for="address" class="label_form"> Address </label>
-                <input type="text" placeholder="Address" name="address" class="input_form" v-model="formData.address">
+                <input type="text" placeholder="Address" name="address" :class="error.eaddress && !(formData.address) ?'red_err':'input_form'" v-model="formData.address">
             </div>
         </div>
         <button class="btn_form blue" type="submit">Create</button>
@@ -67,7 +96,7 @@ import axios from 'axios'
         margin-bottom: 15px;
     }
     .label_form{
-        font-size: 25px;
+        font-size: 20px;
         font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
         font-weight: 400;
     }
@@ -78,6 +107,8 @@ import axios from 'axios'
     .input_form{
         margin: 5px;
         min-width: 300px;
+        border: 2px solid black;
+        border-radius: 5px;
     }
     .btn_form{
         color: white;
@@ -92,5 +123,19 @@ import axios from 'axios'
     .btn_form:hover{
         transform: scale(1.1);
         transition: 0.3s ease-in-out;
+    }
+    .red_err{
+        margin: 5px;
+        min-width: 300px;
+        border-radius: 5px;
+        animation: shake 0.2s ease-in-out 0s 1;
+        box-shadow: 0 0 0.5em red;
+        border: 1px solid red;
+    }
+    @keyframes shake {
+        0% { margin-left: 0rem; }
+        25% { margin-left: 0.5rem; }    
+        75% { margin-left: -0.5rem; }
+        100% { margin-left: 0rem; }
     }
 </style>
